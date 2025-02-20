d2b82626fa8dab4044b3fb2912d6ef35b01ee702
Johan <jfhoyhoy@gmail.com>
2025-02-19 22:47:41 -0800

    FYI | release
    Add support for jsonc in "Recommend"

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,9 @@ All notable changes to the "fyi" extension will be documented here.
 ### Added
 
 - New command `FYI: Add to Workspace Recommendations`: This command appends the FYI extension to the **recommendations** listed in `.vscode/extensions.json`. This further improves the odds that your documentation changes get eyeballed.
+
+## [0.0.9] - 2025-02-19
+
+### Added
+
+- Support `.vscode/extensions.json` files that contain inline comments when running the `FYI: Add to Workspace Recommendations` command.
--- src/recommend.ts
+++ src/recommend.ts
@@ -1,37 +1,47 @@
 import * as vscode from "vscode";
 import path from "node:path";
 import fs from "node:fs";
+import * as jsonc from "jsonc-parser";
 import { EXTENSION_ID, EXTENSION_NAME } from "./constants";
+import selectWorkspaceThenDo from "./selectWorkspaceThenDo";
 
-const addToWorkspaceRecommendations = () => {
-  const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
-  if (!workspaceFolder) {
-    vscode.window.showErrorMessage("No workspace folder found.");
-    return;
-  }
-
-  const vscodeDirPath = path.join(workspaceFolder.uri.fsPath, ".vscode");
+const addToRecommendations = (folderPath: string) => {
+  const vscodeDirPath = path.join(folderPath, ".vscode");
   const extensionsJsonPath = path.join(vscodeDirPath, "extensions.json");
   let extensionsJson: { recommendations?: string[] } = {};
-
+  let errors: jsonc.ParseError[] = [];
+  let content = "{}";
   if (!fs.existsSync(vscodeDirPath)) {
     fs.mkdirSync(vscodeDirPath);
   }
-
   if (fs.existsSync(extensionsJsonPath)) {
-    const content = fs.readFileSync(extensionsJsonPath, "utf-8");
-    extensionsJson = JSON.parse(content);
+    content = fs.readFileSync(extensionsJsonPath, "utf-8");
+    extensionsJson = jsonc.parse(content, errors);
+    if (errors.length > 0) {
+      vscode.window.showErrorMessage(
+        `Error parsing extensions.json: ${
+          jsonc.printParseErrorCode(errors[0].error)
+        }`,
+      );
+      return;
+    }
   }
-
-  if (!extensionsJson.recommendations) {
+  if (!Array.isArray(extensionsJson.recommendations)) {
     extensionsJson.recommendations = [];
   }
-
   if (!extensionsJson.recommendations.includes(EXTENSION_ID)) {
     extensionsJson.recommendations.push(EXTENSION_ID);
+    const edits = jsonc.modify(
+      content,
+      ["recommendations"],
+      extensionsJson.recommendations,
+      { formattingOptions: {} },
+    );
+    const updatedContent = jsonc.applyEdits(content, edits);
     fs.writeFileSync(
       extensionsJsonPath,
-      JSON.stringify(extensionsJson, null, 2),
+      updatedContent,
+      "utf8",
     );
     vscode.window.showInformationMessage(
       `Added ${EXTENSION_NAME} to workspace recommendations.
@@ -42,6 +52,13 @@ const addToWorkspaceRecommendations = () => {
       `${EXTENSION_NAME} is already in workspace recommendations.`,
     );
   }
+};
+
+const addToWorkspaceRecommendations = () => {
+  selectWorkspaceThenDo(
+    `add ${EXTENSION_NAME} to recommendations`,
+    addToRecommendations,
+  );
 };
 
 export default addToWorkspaceRecommendations;
